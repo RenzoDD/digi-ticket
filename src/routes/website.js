939b37e1462ff1fd485169bb2ec4fb43b4f52a3e
Renzo Diaz <renzo.diaz@remadi.net>
2023-01-12 18:19:30 -0500

    Added User login

--- src/routes/website.js
+++ src/routes/website.js
@@ -1,10 +1,29 @@
+const MySQL = require('../utilities/mysql');
+
 const express = require('express');
 const router = express.Router();
 
 // Home
 router.get('/', async function (req, res) {
     var { motd } = require('../settings.json');
-    res.render("home", { code: "/home", motd });
+    res.render("home", { code: "/home", session: req.session, motd });
+});
+
+// Login
+router.get('/login', async function (req, res) {
+    res.render("login", { code: "/login", session: req.session });
+});
+router.post('/login', async function (req, res) {
+    if (req.session.user)
+        return res.redirect('/error/login/already-in');
+
+    var user = await MySQL.Query("CALL Users_Login(?,?)", [req.body.email, req.body.password]);
+
+    if (user.length != 1)
+        return res.redirect('/error/login/invalid-credentials');
+
+    req.session.user = user[0];
+    return res.redirect('/');
 });
 
 module.exports = router;
